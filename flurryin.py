bacca900705e240154e0db31764d0f49ca7001cc
acokikoy <fox_b@nifty.com>
2019-03-02 16:56:15 +0900

    csv読み書き周りのfunctionをflurrynote.pyに吐き出した。

--- flurryin.py
+++ flurryin.py
@@ -1,10 +1,8 @@
 """ Flurry Timeline - 1行ログ処理 """
 from datetime import datetime
-import csv
-import os
 
 
-# import flurrynote
+import flurrynote
 
 F_PATH = 'log.tsv'
 F_ITEMS = ['created_on', 'body', 'tags', 'detail', 'main_task']
@@ -12,19 +10,9 @@ F_ITEMS = ['created_on', 'body', 'tags', 'detail', 'main_task']
 
 def main():
     # ファイルがなければ新規作成して1行目に項目行を書き込む。
-    if not os.path.isfile(F_PATH):
-       with open(F_PATH, 'w', encoding='utf-8', newline='') as f:
-            writer = csv.DictWriter(f, F_ITEMS, delimiter='\t')
-            writer.writeheader()
+    flurrynote.create_note()
 
-    """
-    note:
-        'created_on':   ログ時刻, 
-        'body':         ログ本文, 
-        'main_task':    ログ時点で行なっていたメインタスク
-    """    
     note = {}
-
     while 1:
         str_input = input("なんか気になった？: ")
         if str_input == 'q':
@@ -33,21 +21,20 @@ def main():
         note['created_on'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
         if str_input.startswith('m '):
-            # 「m 」から始まってたら現在のメインタスクを書き換える
+            # 「m 」から始まってたら、その文字列で "現在のメインタスク" を書き換える
             note['main_task'] = str_input[2:]
         else:
             note['body'] = str_input
+            flurrynote.add_note(note) # 今回のログを記録
 
-            with open(F_PATH, 'a', encoding='utf-8', newline='') as f:
-                writer = csv.DictWriter(f, F_ITEMS, delimiter='\t')
-                writer.writerow(note) # 今回のログを記録
-
-
-    with open(F_PATH, 'r', encoding='utf-8') as f:
-        print('お疲れ様でした。今日のふらり〜は次の通りです。\n','*'*20)
-        reader = csv.reader(f, delimiter='\t')
-        for row in reader:
-            print(row)
+    print('お疲れ様でした。今日のふらり〜は次の通りです。\n','*'*20)
+    notes = flurrynote.load_notes()
+    main_task = ''
+    for note in notes:
+        if main_task != note['main_task']:
+            main_task = note['main_task']
+            print(main_task, '-------------------')
+        print(f"\ton {note['created_on']}: {note['body']}")
 
 if __name__ == '__main__':
     main()
---
+++ flurrynote.py
@@ -0,0 +1,80 @@
+""" Flurry Notes 
+notesはdict型（暫定）
+notes = {
+      'c_date': 2019-02-16 16:40:00,
+      'body': '本文', 
+      'tags': 'python, foo, bar', 
+      'detail': '詳しい内容', 
+      'main_task': '本来やってたメインタスク'
+    }
+"""
+import argparse
+import csv
+import os
+
+
+# CONSTANTS
+CSV_PATH = 'log.tsv'
+CSV_ITEMS = ['created_on', 'body', 'tags', 'detail', 'main_task']
+
+def create_note():
+    """ ファイルがなければ新規作成して1行目に項目行を書き込む。"""
+    if not os.path.isfile(CSV_PATH):
+        with open(CSV_PATH, 'w', encoding='utf-8', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, CSV_ITEMS, delimiter='\t')
+            writer.writeheader()
+
+def add_note(note):
+    """ 指定ファイルに、ログを一件追加する """
+    with open(CSV_PATH, 'a', encoding='utf-8', newline='') as csv_file:
+       writer = csv.DictWriter(csv_file, CSV_ITEMS, delimiter='\t')
+       writer.writerow(note) # 今回のログを書き込む
+
+
+def load_notes():
+    """ ファイルから読み出してdisc型のリストとして返す """
+    with open(CSV_PATH, 'r', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter='\t')
+        notes = []
+        for row in reader:
+            notes.append(dict(row))
+        return notes
+
+
+
+def update_notes(path):
+    """ notesを指定パスのファイルに書き出し保存する """
+    print("ファイルに保存しました")
+
+# class FlurryNote:
+#     """
+#     fn = FlurryNote(body)
+#         fn.id (int)　・・・一意な通し番号。編集用に使う
+#         fn.body (str) = body
+#         fn.c_date (datetime)
+#         fn.tags (str)
+#         fn.detail (str)
+#     """
+#     last_num = 0
+
+#     def __init__(self, body, c_date=None, tags='', detail=''):
+#         """ 一意なidを発行する, c_dateが無指定なら現在時刻をセットして、tagとdetailも初期化する """
+#         FlurryNote.last_num += 1
+#         self.id = FlurryNote.last_num
+
+#         icsv_file c_date is None:
+#             self.c_date = datetime.now()
+#         else:
+#             self.c_date = c_date
+
+#         self.body = body
+#         self.tags = tags
+#         self.detail = detail
+
+
+
+#     def edit_note(self, body, tags, detail):
+#         """ 本文、タグ、詳細メモを上書き編集 """
+#         self.body = body
+#         self.tags = tags
+#         self.detail = detail
